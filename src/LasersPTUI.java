f03ba851217cc7cff6129591707d71f56dd3409c
jeb9436 <jeb9436@cs.rit.edu>
2016-04-10 16:40:31 -0400

    Initial commit

---
+++ src/LasersPTUI.java
@@ -0,0 +1,12 @@
+/**
+ * Created by JakeBashaw on 4/10/2016.
+ */
+public class LasersPTUI
+{
+    public static void main(String args[])
+    {
+        System.out.println("My name is Jake Bashaw");
+        System.out.println("My name is Oscar Onyeke");
+        System.out.println("Our project account is p142-11a");
+    }
+}
