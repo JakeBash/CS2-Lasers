acea214a33e254e5c00e9ca2b1a61f74508ab313
oio1606 <oio1606@g.rit.edu>
2016-04-13 16:14:08 -0400

    Initial commit

---
+++ src/LasersPTUI.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Oscar on 4/13/2016.
+ */
+public class LasersPTUI {
+    void name(){
+        System.out.println("My name is Oscar Onyeke");
+        System.out.println("My name is Jake Bashaw");
+        System.out.println("Our project account is p142-11a");
+    }
+}
