edb81ac9275b4092ed51d7c49b77a608b5910f73
jeb9436 <jeb9436@cs.rit.edu>
2016-04-13 16:42:01 -0400

    Added constructor

--- src/LasersPTUI.java
+++ src/LasersPTUI.java
@@ -1,12 +1,41 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * Created by JakeBashaw on 4/10/2016.
  */
 public class LasersPTUI
 {
+    // PRIVATE VARIABLES
+    /** The max row and column size */
+    private int size;
+    /** The board that is being occupied by tents, grass, and trees */
+    private String[][] b;
+    /** A string array of the row looking values */
+    private int rsize;
+    private int csize;
+
     public static void main(String args[])
     {
-        System.out.println("My name is Jake Bashaw");
-        System.out.println("My name is Oscar Onyeke");
-        System.out.println("Our project account is p142-11a");
+
+    }
+
+    public LasersPTUI(String filename) throws FileNotFoundException
+    {
+        Scanner in = new Scanner(new File(filename));
+        rsize = in.nextInt();
+        csize = in.nextInt();
+        in.nextLine();
+        b = new String[rsize][csize];
+        for (int row=0; row < b.length; row++)
+        {
+            for (int col = 0; col < b[row].length; col++)
+            {
+                b[row][col] = in.next();
+            }
+        }
+        in.close();
+        System.out.println(toString());
     }
 }
